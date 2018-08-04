2c65dcc4e0038b6167332b05cb4ba45cfa60df84
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-04 19:56:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens = kittens.concat(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.splice(0, 0, name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.slice(0, kittens.slice(-1));
+  return kittens;
+}
